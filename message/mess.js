23940e4a4a6f97247bcd9858cf4927b524a5c673
AndhikaGanz-404 <94132838+AndhikaGanz-404@users.noreply.github.com>
2023-01-04 21:49:26 +0700

    Add files via upload

---
+++ message/mess.js
@@ -0,0 +1,20 @@
+module.exports.mess = {
+	        regist: '─ 「 *NOT REGISTERED* 」 ─\n\n*ENGLISH*\n_YOU HAVE NOT REGISTERED ON THE DATABASE BOT, TYPE .verify FOR USE BOT_\n\n*INDONESIA*\n_ANDA BELUM TERDAFTAR DI DATABASE BOT, KETIK .daftar UNTUK MENGGUNAKAN BOT_',
+		    dewasa: '─ 「 *WARNING* 」 ─\n\n*ENGLISH*\n_not allowed for minors, if you are an adult, please verify first, type .my18_\n\n*INDONESIA*\n_dilarang untuk anak di bawah umur, jika Anda sudah dewasa, silahkan verifikasi terlebih dahulu, ketik .saya18_',
+		    waitregist: '*ENGLISH*\n_WAIT A MINUTE_\n\n*INDONESIA*\n_TUNGGU SEBENTAR_',
+	        wait: '_Wait Process!..._',
+			success: '_Done, Thanks for use this bot',
+			wrongFormat: '_Format error, try again!_',
+			error: {
+				api: '_Error, apikey is not found!_',
+				stick: '_Error, just try to send a picture!_',
+				Iv: '_Error, try to give the correct link!_'
+			},
+			only: {
+				group: '_Can only be used in groups!_',
+				admin: '_Can only be used by admin!_',
+				premium: '_Can only be used by premium users!_',
+				owner: '_Can only be used by owner!_',
+				Badmin: '_Can only be used if the bot is an admin!_',
+			}
+		}
